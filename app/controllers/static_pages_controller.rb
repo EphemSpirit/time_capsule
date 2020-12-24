8d79d5fae0b4afc0283651e6aac8b611f3cae8ee
Drew Hund <languageinvestigator@gmail.com>
2020-12-24 15:21:33 -0500

    finished initial styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
-  
+  skip_before_action :authenticate_user!
+
   def home
 
   end
+
+  def about
+
+  end
+
 end
